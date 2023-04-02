c839a9bab0ccc79e7575321b945cad77cabdd000
insert <insert@inserts-MacBook-Pro.local>
2023-04-01 21:28:46 -0700

    last time doing that hopefully

--- src/assets/assetIndex.tsx
+++ src/assets/assetIndex.tsx
@@ -4,7 +4,7 @@ import boogieBoard from "./images/boogieBoard.jpg";
 import camera from "./images/camera.jpg"
 import fullThicc from './mediaKits/reineYurkowskiAssets/fullThicc.svg';
 import initialsThicc from "./mediaKits/reineYurkowskiAssets/initialsThicc.svg";
-import linkedinLogo from './mediaKits/LinkedIn-Logos/LinkedIn-Logos/LI-In-Bug.png';
+import linkedinLogo from './mediaKits/LinkedIn-Logos/LinkedIn-Logos/LI-In-Bug2.png';
 import linktreeLogo from './mediaKits/linktree/med.png';
 import roundedSquare from './roundedSquare.svg';
 import ry from './images/ry.jpg';
