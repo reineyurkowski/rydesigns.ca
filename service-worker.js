c31c9470ecded0b5db78cd603b09041dd3b5b7aa
insert <insert@inserts-MacBook-Pro.local>
2023-04-08 00:26:23 -0700

    fixed some loading issues with the webpage

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,29 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('my-cache-name').then(function(cache) {
-      return cache.addAll([
-        // Add the URLs of the assets you want to cache here
-      ]);
-    })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open('my-cache-name').then(function(cache) {
+//       return cache.addAll([
+//         // Add the URLs of the assets you want to cache here
+//       ]);
+//     })
+//   );
+// });
 
-self.addEventListener('activate', function(event) {
-  // Perform any necessary clean-up tasks here
-});
+// self.addEventListener('activate', function(event) {
+//   // Perform any necessary clean-up tasks here
+// });
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/service-worker.js');
+// }
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // Registration failed
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
