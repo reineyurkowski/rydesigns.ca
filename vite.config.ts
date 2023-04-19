b06dd88ba0362d260b875d8b0ac42e8988084764
insert <insert@inserts-MacBook-Pro.local>
2023-04-19 11:28:25 -0700

    added font to vites local directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
-
-// resolve: {
-//   alias: {
-//     '@': path.resolve(__dirname, 'src'),
-//   },
-// },
